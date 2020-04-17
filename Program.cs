f7d6629a2d6a58ce57241e7bc34c3a4f2f365570
Ubuntu <ubuntu@ubuntu.home>
2020-04-17 09:36:06 +0200

    First release

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace rpi001
+{
+    class Program
+    {
+        /*
+           Add GPIO Libraries:
+
+           dotnet add package System.Device.Gpio --source https://dotnetfeed.blob.core.windows.net/dotnet-iot/index.json
+           dotnet add package Iot.Device.Bindings --source https://dotnetfeed.blob.core.windows.net/dotnet-iot/index.json
+
+           Exec this line for use GPIO:
+           
+           sudo chmod 777 /dev/gpiomem
+        */
+        public static int GPIO17=17;
+        static void Main(string[] args)
+        {
+            GpioController controller = new GpioController ();
+            controller.OpenPin (GPIO17,PinMode.Output);
+            int timeUp = 5000;
+            int timeLow = 5000;
+
+            Console.WriteLine("Hello World!");
+            while(true)
+           {
+               Console.WriteLine("Line Up");
+               controller.Write(GPIO17,PinValue.High);
+               Thread.Sleep(timeUp);
+               Console.WriteLine("Line Down");
+               controller.Write(GPIO17,PinValue.Low);
+               Thread.Sleep(timeLow);
+           } 
+        }
+    }
+}
